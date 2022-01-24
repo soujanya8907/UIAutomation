6480485f18f44ed6d8f001adac9205063b74d696
soujanya8907 <98277294+soujanya8907@users.noreply.github.com>
2022-01-24 17:17:05 -0500

    Update LabCorp_Careers_StepDef.java

--- src/test/java/ui/automation/StepDef/LabCorp_Careers_StepDef.java
+++ src/test/java/ui/automation/StepDef/LabCorp_Careers_StepDef.java
@@ -6,20 +6,20 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LabCorp_Careers_StepDef {
-  @Given("^you are in Given annotation$")
+  @Given("^Launch LabCorp website$")
   public void given() throws Throwable {
   }
 
-  @When("^you are in When annotation$")
+  @When("^LabCorp Site Launched Click on Careers link$")
   public void when() throws Throwable {
   }
 
-  @Then("^you are in Then annotation$")
-  public void then() throws Throwable {
+  @And("^Search for a open position and click on Search$")
+  public void and() throws Throwable {
   }
 
-  @And("^you are in And annotation$")
-  public void and() throws Throwable {
+  @Then("^Then Validate job details and click on Job number$")
+  public void then() throws Throwable {
   }
 
 }
